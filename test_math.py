3f91d3f55507775f004acaef87684789fbace493
Musfiray <Musfira.Waseem96@student.qcc.cuny.edu>
2026-04-15 19:22:02 +0000

    Test Cases for Musfira's Core F by Musfira

--- test_math.py
+++ test_math.py
@@ -1,4 +1,7 @@
-from my_math import factorial
+
+import unittest
+
+from my_math import factorial, sqrt
 
 def test_factorial1():
     assert factorial(5) == 120
@@ -12,3 +15,17 @@ def test_factorial3():
         assert False
     except ValueError:
         assert True
+
+# Musfira's Core Function Unittest by Musfira 
+def test_sqrt_typical():
+
+    result = sqrt(9)
+    assert abs(result - 3) < 0.0001
+
+def test_sqrt_edge():
+
+    assert sqrt(0) == 0
+
+def test_sqrt_invalid():
+
+    assert sqrt(-1) is None
